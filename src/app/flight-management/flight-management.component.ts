7ce247eb7147d2c36b6dbb898f9e7675eef76557
Grecia Eravinash <trunghoangnguyendaihoc2021@gmail.com>
2023-10-16 15:07:48 +0700

    initial commit

---
+++ src/app/flight-management/flight-management.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-management',
+  templateUrl: './flight-management.component.html',
+  styleUrls: ['./flight-management.component.scss']
+})
+export class FlightManagementComponent {
+
+}
---
+++ src/app/flight-management/flight-management.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlightManagementComponent } from './flight-management.component';
+
+
+
+@NgModule({
+  declarations: [
+    FlightManagementComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FlightManagementModule { }
