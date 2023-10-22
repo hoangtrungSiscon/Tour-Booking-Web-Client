4fad2a49d5ee2faa13e9821d0767b1c8f64f23cf
NguyenHoangTrung <111077847+hoangtrungSiscon@users.noreply.github.com>
2023-10-22 20:50:28 +0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,74 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// const routes: Routes = [];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminNavigationComponent } from './components/admin-navigation/admin-navigation.component';
+import { AdminFlightManagementComponent } from './pages/admin-flight-management/admin-flight-management.component';
+import { AdminTicketManagementComponent } from './pages/admin-ticket-management/admin-ticket-management.component';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
+import { EditFlightComponent } from './pages/edit-flight/edit-flight.component';
+import { AddFlightComponent } from './pages/add-flight/add-flight.component';
+import { GuestManagementComponent } from './pages/guest-management/guest-management.component';
+import { AdminNaviComponent } from './components/admin-navi/admin-navi.component';
+import { NewAccountComponent } from './pages/new-account/new-account.component';
+import { StatisticalComponent } from './pages/statistical/statistical.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { PieChartsComponent } from './components/pie-charts/pie-charts.component';
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+import { LineChartsComponent } from './components/line-charts-eco/line-charts.component';
+import { LineChartsBSNComponent } from './components/line-charts-bsn/line-charts-bsn.component';
+import { ColumnChartsComponent } from './components/column-charts/column-charts.component';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [
+    AppComponent,
+    AdminNavigationComponent,
+    AdminFlightManagementComponent,
+    AdminTicketManagementComponent,
+    routingComponents,
+    AdminDashboardComponent,
+    EditFlightComponent,
+    AddFlightComponent,
+    GuestManagementComponent,
+    AdminNaviComponent,
+    NewAccountComponent,
+    StatisticalComponent,
+    PieChartsComponent,
+    LineChartsComponent,
+    LineChartsBSNComponent,
+    ColumnChartsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatTableModule,
+    MatPaginatorModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSlideToggleModule,
+    CanvasJSAngularChartsModule,
+    MatGridListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppRoutingModule { }
+export class AppModule { }
