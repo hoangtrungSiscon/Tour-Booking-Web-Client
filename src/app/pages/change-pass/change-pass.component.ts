817d1b97fe7ae56f9ec7c6210774cdf98ce1a569
PDai03 <4701104057@student.hcmue.edu.vn>
2023-10-23 21:09:30 +0700

    updated

--- src/app/pages/change-pass/change-pass.component.ts
+++ src/app/pages/change-pass/change-pass.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+import Swal from 'sweetalert2'
+
 @Component({
   selector: 'app-change-pass',
   templateUrl: './change-pass.component.html',
   styleUrls: ['./change-pass.component.scss']
 })
 export class ChangePassComponent {
+  visible:boolean = true;
+  changetype:boolean = true;
+  hide = true;
+  viewpass(){
+    this.visible = !this.visible;
+    this.changetype = !this.changetype;
 
+  }
+  alertSuccess(){
+    Swal.fire(
+      'Good job!',
+      'Bạn đã đổi mật khẩu thành công',
+      'success'
+    )
+  }
 }
