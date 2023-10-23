ffdd521cda67050013c6a5ac2b427b962c666bc8
NguyenHoangTrung <yamitsukichikinako7777@gmail.com>
2023-10-23 09:37:01 +0700

    phuongdai-pre-merge

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TourBookingWeb'`, () => {
+  it(`should have as title 'Flight-Booking'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TourBookingWeb');
+    expect(app.title).toEqual('Flight-Booking');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('TourBookingWeb app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Flight-Booking app is running!');
   });
 });
---
+++ src/app/pages/view-profile/view-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-profile',
+  templateUrl: './view-profile.component.html',
+  styleUrls: ['./view-profile.component.scss']
+})
+export class ViewProfileComponent {
+  password: string = '';
+  showPassword: boolean = false;
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+}
