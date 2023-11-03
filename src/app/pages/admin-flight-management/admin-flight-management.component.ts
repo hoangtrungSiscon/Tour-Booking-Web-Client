d1fbe7e57e215ac40ecbbbefd2a7235e0ad31754
NguyenHoangTrung <yamitsukichikinako7777@gmail.com>
2023-11-03 21:40:39 +0700

    update

--- src/app/pages/admin-flight-management/admin-flight-management.component.ts
+++ src/app/pages/admin-flight-management/admin-flight-management.component.ts
@@ -45,5 +45,6 @@ export class AdminFlightManagementComponent {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    console.log(this.dataSource.data)
   }
 }
