25ef3fa24993aecaa1ec920be16805ddf2aae21e
NguyenHoangTrung <yamitsukichikinako7777@gmail.com>
2023-11-05 13:27:39 +0700

    add ADD method to add-flight component

--- src/app/pages/add-flight/add-flight.component.ts
+++ src/app/pages/add-flight/add-flight.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { NgSelectOption } from '@angular/forms';
 import { FlightDetails } from 'src/app/models/FlightDetailModel';
@@ -27,7 +28,7 @@ export class AddFlightComponent implements OnInit {
   public flightTime_Minute: string = '00';
   public flightTime_Second: string = '00';
   public planeList: any[] = []
-  constructor(private flightService: FlightApiService, private planeService: PlaneApiService) {
+  constructor(private flightService: FlightApiService, private planeService: PlaneApiService, private router: Router) {
 
   }
   ngOnInit(): void {
@@ -37,13 +38,6 @@ export class AddFlightComponent implements OnInit {
       console.log(this.planeList)
     })
   }
-
-  // updateTenMayBay() {
-  //   const selectedPlane = this.planeList.find(plane => plane.maMayBay === this.selectedMaMayBay);
-  //   if (selectedPlane) {
-  //       this.selectedTenMayBay = selectedPlane.tenMayBay;
-  //   }
-  // }
 
   updatePlaneName(event: any, id: any) : void{
     const element = this.planeList.find(element => element.maMayBay == id);
@@ -55,23 +49,8 @@ export class AddFlightComponent implements OnInit {
   }
 
   addFlight(){
-    // this.service.addFlight(this.addFlightRequest).subscribe();
-    // this.addFlightRequest = {
-    //   maChuyenBay: '',
-    //   maMayBay: '',
-    //   tenMayBay: '',
-    //   noiXuatPhat: '',
-    //   noiDen: '',
-    //   ngayXuatPhat: '',
-    //   gioBay: '',
-    //   soLuongVeBsn: '',
-    //   soLuongVeEco: '',
-    //   donGia: '',
-    // }
-    // this.addFlightRequest.donGia = parseInt(this.addFlightRequest.donGia);
-    // this.addFlightRequest.soLuongVeBsn = parseInt(this.addFlightRequest.soLuongVeBsn);
     if (
-      // this.addFlightRequest.maChuyenBay == '' ||
+      this.addFlightRequest.maChuyenBay == '' ||
       this.addFlightRequest.maMayBay == '' ||
       this.addFlightRequest.tenMayBay == '' ||
       this.addFlightRequest.noiXuatPhat == '' ||
@@ -85,48 +64,51 @@ export class AddFlightComponent implements OnInit {
       // 
       Swal.fire({
         icon: 'error',
-        title: 'Thêm chuyến bay không thành công!',
+        title: 'Chưa nhập đầy đủ thông tin!',
         text: 'Vui lòng nhập đầy đủ thông tin chuyến bay!',
         // footer: '<a href="">Tạo sao tôi lại gặp lỗi này?</a>'
       })
     }
-    this.addFlightRequest.gioBay = this.flightTime_Hour.toString().padStart(2, '0') +":"+ this.flightTime_Minute.toString().padStart(2, '0') +":"+ this.flightTime_Second.toString().padStart(2, '0');
-    this.addFlightRequest.donGia = this.addFlightRequest.donGia.replace(/,/g, '');
-    // this.flightService.addFlight(this.addFlightRequest).subscribe();
-    console.log(this.addFlightRequest);
-    Swal.fire({
-      title: 'Thêm thông tin chuyến bay này?',
-      text: "Bạn có muốn thực hiện việc thêm thông tin chuyến bay này không?",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Thêm chuyến bay',
-      cancelButtonText: 'Hủy'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.flightService.addFlight(this.addFlightRequest).subscribe(
-          () => {
-            console.log('Deleted successfully');
-            Swal.fire(
-              'Đã thêm thông tin chuyến bay!',
-              'Thêm thông tin chuyến bay thành công.',
-              'success'
-            ).then(() => {
-              window.location.reload();
-            })
-          },
-          (error) => {
-            Swal.fire(
-              'Thêm chuyến bay không thành công!',
-              'Đã xảy ra lỗi khi thực hiện việc thông tin chuyến bay này.',
-              'error'
-            );
-          }
-        );
-        
-      }
-    })
+    else {
+      this.addFlightRequest.gioBay = this.flightTime_Hour.toString().padStart(2, '0') +":"+ this.flightTime_Minute.toString().padStart(2, '0') +":"+ this.flightTime_Second.toString().padStart(2, '0');
+      this.addFlightRequest.donGia = this.addFlightRequest.donGia.replace(/,/g, '');
+      // this.flightService.addFlight(this.addFlightRequest).subscribe();
+      console.log(this.addFlightRequest);
+      Swal.fire({
+        title: 'Thêm thông tin chuyến bay này?',
+        text: "Bạn có muốn thực hiện việc thêm thông tin chuyến bay này không?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Thêm chuyến bay',
+        cancelButtonText: 'Hủy'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.flightService.addFlight(this.addFlightRequest).subscribe(
+            () => {
+              console.log('Deleted successfully');
+              Swal.fire(
+                'Đã thêm thông tin chuyến bay!',
+                'Thêm thông tin chuyến bay thành công.',
+                'success'
+              ).then(() => {
+                this.router.navigate(['/admin-flight-management']);
+              })
+            },
+            (error) => {
+              Swal.fire(
+                'Thêm chuyến bay không thành công!',
+                'Đã xảy ra lỗi khi thực hiện việc thông tin chuyến bay này.',
+                'error'
+              );
+            }
+          );
+          
+        }
+      })
+    }
+    
     // console.log(this.)
   }
 
