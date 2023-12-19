b3c141446a48de3ef8b8c291df31ef4917ccef80
KMinhMilay <plkminh.c918@gmail.com>
2023-12-19 20:44:21 +0700

    update

--- src/app/pages/booking-history/booking-history.component.ts
+++ src/app/pages/booking-history/booking-history.component.ts
@@ -3,12 +3,14 @@ import { TicketHistoryApiService } from 'src/app/shared/services/ticket-history-
 import { AuthService } from '../../shared/services/auth.service';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { da } from 'date-fns/locale';
 @Component({
   selector: 'app-booking-history',
   templateUrl: './booking-history.component.html',
   styleUrls: ['./booking-history.component.scss']
 })
 export class BookingHistoryComponent {
+  public tickets: any[] = [];
   constructor(
     private tickethistoryapi:TicketHistoryApiService,
     private authService:AuthService,
@@ -16,10 +18,12 @@ export class BookingHistoryComponent {
   ){}
   async ngOnInit(){
     // this.form=this.createForm();
+    // console.log(this.authService.getUserId())
     this.authService.getUserId().subscribe((id) => {
       if(id !=0 ){
         this.tickethistoryapi.getTicketHistoryList(id).subscribe((data)=>{
           console.log(data)
+          this.tickets=data;
           // this.form.patchValue(data);
         });
 
