6f804ddad29dfed5cfd1ca7af40ec47c8d777185
KMinhMilay <plkminh.c918@gmail.com>
2023-12-22 19:30:59 +0700

    update validator

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { debounceTime } from 'rxjs';
 import Swal from 'sweetalert2';
 import { AuthService } from '../../shared/services/auth.service';
 import { Router } from '@angular/router';
-
+import { Validator } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -24,7 +24,7 @@ export class SignUpComponent implements OnInit {
     this.form=this.createForm();
     this.checkPass.valueChanges.pipe(debounceTime(300)).subscribe((data) => {
       if(this.form.get('matKhau').value != data){
-        this.message="Error message"
+        this.message="Mật khẩu không trùng nhau, vui lòng nhập lại"
       }
       else{
         this.message = ""
@@ -34,12 +34,17 @@ export class SignUpComponent implements OnInit {
   
   createForm(){
     return this.formBuilder.group({
-      taiKhoan1: [''],
-      matKhau: [''],
-      tenKH: [''],
-      sdt: [''],
-      gamilKH: [''],
-      phai: ['']
+      taiKhoan1: ['',[Validators.required]],
+      matKhau: ['',[Validators.required]],
+      tenKH: ['',[Validators.required]],
+      sdt: ['',[
+        Validators.pattern(/^\d+$/), // Chỉ chấp nhận số
+        Validators.minLength(9),     // Độ dài ít nhất là 9
+        Validators.maxLength(11),
+        Validators.required    // Độ dài tối đa là 11
+      ]],
+      gamilKH: ['',[Validators.required,Validators.email]],
+      phai: ['',Validators.required]
     })  
     
   }
