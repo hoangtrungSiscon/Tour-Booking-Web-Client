7ea85980a838400f49e2aa03801dfa43fb0cf424
NguyenHoangTrung <yamitsukichikinako7777@gmail.com>
2023-12-23 14:08:18 +0700

    navigate to booking-ticket

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -28,46 +28,8 @@ export class HomeComponent {
       origin: this.departure,
       destination: this.destination
     }
+    console.log(data)
     this.transferDataService.setData(data)
     this.router.navigate(['/booking-ticket']);
   }
 }
-// <div class="card" style="height: 300px;padding: 5px;border-radius: 20px;border: 2px solid #146c94;">
-// <div class="card-body div-center" style="flex-direction: column;">
-//   <h5 class="card-title">{{newestflights[0].noiXuatPhat}} - {{newestflights[0].noiDen}}</h5>
-//   <p class="card-text"> 
-//     Mã chuyến bay: {{newestflights[0].maChuyenBay}} - Máy bay: {{newestflights[0].tenMayBay}}
-//     <br>
-//     Ngày xuất phát: {{newestflights[0].ngayXuatPhat | date : "dd/MM/yyyy"}} - Giờ bay: {{newestflights[0].gioBay}}
-//     <br>
-//     Đơn giá: {{newestflights[0].donGia}}
-//   </p>
-//   <a href class="btn btn-primary">Go somewhere</a>
-// </div>
-// </div>
-// <div class="card" style="height: 300px;padding: 5px;border-radius: 20px;border: 2px solid #146c94;">
-// <div class="card-body div-center" style="flex-direction: column;">
-//   <h5 class="card-title">{{newestflights[1].noiXuatPhat}} - {{newestflights[1].noiDen}}</h5>
-//   <p class="card-text"> 
-//     Mã chuyến bay: {{newestflights[1].maChuyenBay}} - Máy bay: {{newestflights[1].tenMayBay}}
-//     <br>
-//     Ngày xuất phát: {{newestflights[1].ngayXuatPhat | date : "dd/MM/yyyy"}} - Giờ bay: {{newestflights[1].gioBay}}
-//     <br>
-//     Đơn giá: {{newestflights[1].donGia}}
-//   </p>
-//   <a href class="btn btn-primary">Go somewhere</a>
-// </div>
-// </div>
-// <div class="card" style="height: 300px;padding: 5px;border-radius: 20px;border: 2px solid #146c94;">
-// <div class="card-body div-center" style="flex-direction: column;">
-//   <h5 class="card-title">{{newestflights[2]?.noiXuatPhat}} - {{newestflights[2].noiDen}}</h5>
-//   <p class="card-text"> 
-//     Mã chuyến bay: {{newestflights[2].maChuyenBay}} - Máy bay: {{newestflights[2].tenMayBay}}
-//     <br>
-//     Ngày xuất phát: {{newestflights[2].ngayXuatPhat | date : "dd/MM/yyyy"}} - Giờ bay: {{newestflights[2].gioBay}}
-//     <br>
-//     Đơn giá: {{newestflights[2].donGia}}
-//   </p>
-//   <a href class="btn btn-primary">Go somewhere</a>
-// </div>
-// </div>
