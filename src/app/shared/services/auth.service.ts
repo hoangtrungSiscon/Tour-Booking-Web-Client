b47e19b99e34476150f47bb0dfae41b480be84d6
NguyenHoangTrung <yamitsukichikinako7777@gmail.com>
2024-01-10 22:35:30 +0700

    update

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -31,30 +31,75 @@ export class AuthService {
     return localStorage.getItem('token');
   }
   storeData(token : any, mataikhoan : any, vaitro : any): void{
-    localStorage.setItem('token', token);
-    localStorage.setItem('mataikhoan', mataikhoan.toString())
-    localStorage.setItem('vaitro', vaitro.toString());
+    // localStorage.setItem('token', token);
+    // localStorage.setItem('mataikhoan', mataikhoan.toString())
+    // localStorage.setItem('vaitro', vaitro.toString());
+    const dataToStore = {
+      token,
+      mataikhoan,
+      vaitro,
+      expirationTime: new Date().getTime() + 7 * 24 * 60 * 60 * 1000 // 7 ngày
+    };
+  
+    // Lưu đối tượng vào local storage
+    localStorage.setItem('userData', JSON.stringify(dataToStore));
   }
+  getData(): { token: any, mataikhoan: any, vaitro: any } | null {
+    const storedData = localStorage.getItem('userData');
+  
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+      const currentTime = new Date().getTime();
+  
+      if (currentTime < parsedData.expirationTime) {
+        // Dữ liệu vẫn còn hiệu lực
+        return {
+          token: parsedData.token,
+          mataikhoan: parsedData.mataikhoan,
+          vaitro: parsedData.vaitro
+        };
+      } else {
+        // Dữ liệu đã hết hạn
+        localStorage.removeItem('userData');
+      }
+    }
+  
+    // Không có dữ liệu hoặc dữ liệu đã hết hạn
+    return null;
+  }
+  
   logout(): void {
-    localStorage.removeItem('token');
-    localStorage.removeItem('mataikhoan');
-    localStorage.removeItem('vaitro');
+    // localStorage.removeItem('token');
+    // localStorage.removeItem('mataikhoan');
+    // localStorage.removeItem('vaitro');
+    localStorage.removeItem('userData');
   }
+  // isLoggedIn(): boolean {
+  //   return !!localStorage.getItem('token');
+  // }
   isLoggedIn(): boolean {
-    return !!localStorage.getItem('token');
+    const userData = this.getData();
+    return !!userData
   }
+  
 
   isUser(): boolean {
-    if (!localStorage.getItem('token')) return false;
-    else return localStorage.getItem('vaitro') == '1';
+    // if (!localStorage.getItem('token')) return false;
+    // else return localStorage.getItem('vaitro') == '1';
+    const userData = this.getData();
+    return !!userData && userData.vaitro == '1';
   }
   isAdmin(): boolean {
-    if (!localStorage.getItem('token')) return false;
-    else return localStorage.getItem('vaitro') == '0';
+    // if (!localStorage.getItem('token')) return false;
+    // else return localStorage.getItem('vaitro') == '0';
+    const userData = this.getData();
+    return !!userData && userData.vaitro == '0';
   }
   
   public thisAccountId() : number{
-    return !localStorage.getItem('mataikhoan') ? 0 : parseInt(localStorage.getItem('mataikhoan')!)
+    // return !localStorage.getItem('mataikhoan') ? 0 : parseInt(localStorage.getItem('mataikhoan')!)
+    const userData = this.getData();
+    return !!userData ? parseInt(userData.mataikhoan) : 0
   }
 
   public register(request: any): Observable<any> {
