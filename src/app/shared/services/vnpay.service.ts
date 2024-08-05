1a0548f78d8bfbf65997e05a968cafbbb7455c47
Trung Siscon <trunghoangnguyen.it@gmail.com>
2024-08-05 12:02:31 +0700

    add vnpay service and logo

---
+++ src/app/shared/services/vnpay.service.ts
@@ -0,0 +1,23 @@
+import { PaymentInfo } from './../models/paymentInfo';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VnpayService {
+
+  protected url: string = 'https://localhost:7000/api/VnPay';
+
+  constructor(protected http: HttpClient) { }
+
+  public createPaymentURL(paymentInfo: PaymentInfo, id: number): Observable<any> {
+    const params = new HttpParams()
+    .set('OrderType', paymentInfo.orderType)
+    .set('Amount', paymentInfo.amount)
+    .set('OrderDescription', paymentInfo.orderDescription)
+    .set('Name', paymentInfo.name);
+    return this.http.get<any>(this.url+`/CreatePaymentUrl?id=${id}`, {params});
+  }
+}
